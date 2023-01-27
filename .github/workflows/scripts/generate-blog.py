124e4ca4dc57f73d4242ac3fd176b7d6df418076
Yuki Hattori <15963767+yuhattor@users.noreply.github.com>
2023-01-27 13:11:33 +0900

    Update

--- .github/workflows/scripts/generate-blog.py
+++ .github/workflows/scripts/generate-blog.py
@@ -22,9 +22,6 @@ def translate_with_deepl(api_key: str, content: str, is_xml: bool = False):
   data = result.json()
   return data["translations"][0]["text"]
 
-def translate_with_deepl_stub(api_key: str, content: str, is_xml: bool = False):
-  return content
-
 def format_date(published_date: str):
   date_string = published_date
   date_object = datetime.strptime(date_string, "%a, %d %b %Y %H:%M:%S %z")
@@ -41,13 +38,13 @@ author = re.search(re.compile(r"## Author\n(.+?)\n\n## Raw Content") , text).gro
 content = re.search(r"## Raw Content(.*)```", text, re.DOTALL).group(1)
 
 blog_content = f"""---
-title: { translate_with_deepl_stub(api_key, title) }
+title: { translate_with_deepl(api_key, title) }
 englishtitle: { title }
 date: { published_date }
 cardurl: { link }
 author: { author }
 ---
-{ translate_with_deepl_stub(api_key, content, is_xml=True) }
+{ translate_with_deepl(api_key, content, is_xml=True) }
 """
 item = f"{published_date}-{re.sub(r'[^a-z0-9]', '-', title.lower())}"
 with open(f"content/changelog/{item}.md", 'w') as f:
