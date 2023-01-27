052e24f82ed18ad6869f1aec4b51cdc248569460
Yuki Hattori <15963767+yuhattor@users.noreply.github.com>
2023-01-27 17:48:43 +0900

    Update Blog gen pipeline

--- .github/workflows/scripts/generate-githubblog.py
+++ .github/workflows/scripts/generate-githubblog.py
@@ -3,6 +3,7 @@ import sys
 import json
 import requests
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 # Get Arguments 
 api_key = sys.argv[1] # DeepL API key
@@ -29,32 +30,45 @@ def format_date(published_date: str):
   date_object = datetime.strptime(date_string, "%a, %d %b %Y %H:%M:%S %z")
   return date_object.strftime("%Y-%m-%d")
 
+
+def get_cover_image_url(url):
+  response = requests.get(url)
+  html = response.content
+  soup = BeautifulSoup(html, 'html.parser')
+  img = soup.find('img', class_="cover-image")
+  return img
+
 # Get Issue Body
 text = requests.get(f"https://api.github.com/repos/{repository}/issues/{issue_id}").json()["body"]
 
 # Get Items
-title = re.search(re.compile(r"## Title\n(.+?)\n\n## Link") , text).group(1) # Get title from issue body
+title = re.search(re.compile(r"## Title\n(.+?)\n\n## Subtitle") , text).group(1) # Get title from issue body
+subtitle = re.search(re.compile(r"## Subtitle\n(.+?)\n\n## Link") , text).group(1) # Get subtitle from issue body
 link = re.search(re.compile(r"## Link\n(.+?)\n\n## Published date") , text).group(1) # Get link from issue body
 published_date = format_date(re.search(re.compile(r"## Published date\n(.+?)\n\n## Author") , text).group(1)) # Get published date from issue body
-author = re.search(re.compile(r"## Author\n(.+?)\n\n## Description") , text).group(1) # Get author from issue body
+author = re.search(re.compile(r"## Author\n(.+?)\n\n## Category") , text).group(1) # Get author from issue body
+category = re.search(re.compile(r"## Category\n(.+?)\n\n## Description") , text).group(1) # Get author from issue body
 description = re.search(re.compile(r"## Description\n(.+?)\n") , text).group(1) # Get description from issue body
 content = re.search(r"## Raw Content(.*)```", text, re.DOTALL).group(1) # Get content from issue body
 
-# format the content for changelog
-changelog_content = f"""---
+# format the content for githubblog
+githubblog_content = f"""---
 title: { translate_with_deepl(api_key, title) }
+subtitle: { translate_with_deepl(api_key, subtitle) }
+englishsubtitle: { subtitle }
 englishtitle: { title }
 date: { published_date }
 cardurl: { link }
 author: { author }
 description: { description }
+coverimage: { get_cover_image_url(link).get("src") }
+category: { category }
 ---
 { translate_with_deepl(api_key, content, is_xml=True) }
 """
 
 # write the content to a file
 item = f"{published_date}-{re.sub(r'[^a-z0-9]', '-', title.lower())}"
-with open(f"content/changelog/{item}.md", 'w') as f:
-  f.write(changelog_content)
+with open(f"content/githubblog/{item}.md", 'w') as f:
+  f.write(githubblog_content)
 
-print(item)
