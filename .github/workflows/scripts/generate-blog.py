762d564587199ef75d4285450b0ac82a1cd9d9b3
Yuki Hattori <15963767+yuhattor@users.noreply.github.com>
2023-01-27 12:58:21 +0900

    update

--- .github/workflows/scripts/generate-blog.py
+++ .github/workflows/scripts/generate-blog.py
@@ -31,7 +31,7 @@ def format_date(published_date: str):
   return date_object.strftime("%Y-%m-%d")
 
 # Get Issue Body
-print() requests.get(f"https://api.github.com/repos/{repository}/issues/{issue_id}").json())
+print(requests.get(f"https://api.github.com/repos/{repository}/issues/{issue_id}").json())
 text = requests.get(f"https://api.github.com/repos/{repository}/issues/{issue_id}").json()["body"]
 
 # Get Items
