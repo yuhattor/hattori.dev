d25a1aa335a337a37d7cd6ce82b1572ce153bcd5
Yuki Hattori <15963767+yuhattor@users.noreply.github.com>
2023-02-04 14:46:08 +0900

    Update

--- .github/workflows/scripts/generate-changelog.py
+++ .github/workflows/scripts/generate-changelog.py
@@ -56,7 +56,9 @@ def summarize(text, openai_api_key):
       "max_tokens": 50
   }
   
-  response = requests.post(url, headers=headers, data=json.dumps(data).encode('utf8', 'ignore').decode('utf8'))
+  payload = json.dumps(data,ensure_ascii=False).encode('utf-8').decode('unicode-escape')
+  response = requests.post(url, headers=headers, data=payload)
+
   message = response['choices'][0]['text']
   return message
 
